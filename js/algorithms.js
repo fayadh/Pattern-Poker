039eedefe78f1705c208e458dfcee46e82e134ed
fayadh <fayadh.almosawi@gmail.com>
2015-06-20 22:41:06 -0400

    finished the linear card algo

--- js/algorithms.js
+++ js/algorithms.js
@@ -17,47 +17,111 @@ var pfac = function() {
 	this.multiply = function(amount) { return this.initial = this.initial * amount }
 	this.divide = function(amount) { return this.intial = this.initial / amount } 
 	this.keys = ['add', 'subtract', 'multiply', 'divide']
+	this.operators = ['+', '-', '/', '*']
 	this.possibilities = function(n1, n2) {
-
 		operators = ['+', '-', '/', '*']
+		//conditional count
+		var count = 0
+		var disc = []
 		for (var i = 0; i < operators.length; i++) {
-				console.log(operators[i])
-				for (var j = 0; j < 10; j++) {
-					s = n1+operators[i]+j+'=='+n2;
-					console.log(eval(s))
-				};
+			for (var j = 0; j < 10; j++) {
+				//the operation string
+				s = n1+this.operators[i]+j+'=='+n2;
+				if (eval(s) == true) {
+					count++
+					disc.push(s)
+				}
+			};
 		};
+		return disc
 	}
 	this.linear = function() {
+			//so that it creates a new card every time
 			p = new pfac
 			this.nbounds = 10
 			keys = ['add', 'subtract', 'multiply', 'divide']
 			//main series array
 			series = []
+
+			//solutions
+			series[1] = []
+			solution = series[1]
+
 			init_rand = Math.floor(Math.random() * this.nbounds)
 
-			//setup a random first number and push it to the main series
-			p.initial = init_rand
-			series[0] = p.initial
+			//Dumps Main
+			var series_operators = [] 
+			var test_condition = 0
 
-			for (var i = 1; i < 4; i++) {
-				//choose a random operation set
-				randk = rand(0, keys.length)
-				randa = rand(1, this.nbounds)
-				k = [keys[randk], randa]
-				console.log(p)
-				console.log(k[0])
-				p[k[0]](k[1])
-				series[i] = p.initial
+			//THE GUIDE
+			//we need to make sure that the guide has 0 black holes. So we create a while loop, that only returns a series with 0 black holes.
+			while(test_condition == 0) {
+				//main variable
+				series[0] = []
+				var guide = series[0]
+
+				//black holes
+				var black_holes = 0
+
+				//Create a random four element series 
+				//setup a random first number and push it to the main series
+				p.initial = init_rand
+				guide[0] = [p.initial]
+				guide[0].push("undefined")
+
+				//append 3 random elements to the guide 
+				for (var i = 1; i < 4; i++) {
+					//choose a random operation set
+					randk = rand(0, keys.length - 1)
+					randa = rand(1, this.nbounds)
+
+					//set operation
+					op = [keys[randk], randa]
+
+					//perform it
+					p[op[0]](op[1])
+
+					//log to the guide array
+					guide[i] = [p.initial, op]
+
+					//record all black holes
+					examined_element = this.possibilities(guide[i - 1][0], guide[i][0])
+					if (examined_element.length > 1) { black_holes++ }
+				}
+	
+				//if you find a black hole, report it.
+				if(black_holes > 0) {
+					//reset the guide. start over.
+					guide = []
+				} else {
+					//exit the loop
+					console.log(guide)
+					test_condition = 1
+				}
 			}
 
-			//Now it's time to decide what kind of card this if going to be. 
-			//There are three types of primary cards. Linear, Single, Multiple. 
-			// console.log([keys[randk], randa])
-			// console.log(series)
+			//choose an new random variable
+			init_rand = rand(1, 10)
+			solution[0] = init_rand
 
+			//reset the inital variable
+			p.initial = init_rand
+
+			//SOLUTION SPACE
+			for (var i = 1; i < 4; i++) {
+				//find the specific operation
+				guide = series[0]
+				key = series[0][i][1][0]
+				amount = series[0][i][1][1]
+				op = [key, amount]
+
+				//perform the operation
+				p[key](amount)
+
+				//append to solution set
+				solution[i] = p.initial
+			};
 		}
-		this.fashion = 10
 	}
 
 genP = new pfac;
---
+++ js/docs.md
@@ -0,0 +1,52 @@
+GLOBAL DOCS
+
+Welcome to Pattern Poker! Poker with a bit of ill-fate and brain power. 
+- Note: only playable on a computer.
+
+Rules!
+
+Players: 2
+
+1) At the start of each round, each player receives a total of three cards.
+
+
+* There are three card types: 
+* linear, single, multiverse.
+
+Anatomy of a card:
+
+	------------
+| 						 |
+| 			3			 |
+| 			5			 |
+| 			8			 |
+| 			10		 |
+| 						 |
+| 			-		 	 |
+|              |
+| 		 	5			 |
+| 			7			 |
+| 			10		 |
+| 			__		 |
+|              |
+	------------
+
+The top section is called the guide. It identifies the pattern that we are going to be using to guess the solution.
+The bottom section is the solution space. It's where the user has to attempt to guess the correct pattern.
+
+The game is played by using the guides as a way of correctly identifying each of operations performed on each step.
+
+Types of cards: 
+* A blackhole is an element with multiple operation sets
+
+* linear cards: 0 blackholes -- only one possible operation per step. << easiest to solve.
+* single: 1 black hole
+* multiverse: 2 black holes
+* suicide: 3 black holes. << nearly impossible. 
+
+* there's special hacks that people can apply 
+- Joker: solves the puzzle. 
+
+
+Round: 
+
